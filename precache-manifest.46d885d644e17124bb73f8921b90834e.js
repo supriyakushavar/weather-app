8bb4228e08f730dcb72c9d84c280298cc9c24700
supriyakushavar <supriyakushavar@gmail.com>
2019-06-20 11:35:33 +0200

    Updates

---
+++ precache-manifest.46d885d644e17124bb73f8921b90834e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ca515ce191215558ae81bb46c1c12e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6d9ce8d102d84a7713b2",
+    "url": "/weather-app/static/css/main.280da949.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/weather-app/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "6d9ce8d102d84a7713b2",
+    "url": "/weather-app/static/js/main.1c1d7a7a.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
